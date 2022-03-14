58f483aab58a74f16c8abd3d09e8a384b7ff509d
Jonathan Poelman <jonathan.poelman@protonmail.com>
2022-03-14 09:54:30 +0100

    remove old restore session method from application route

--- tests/dummy/app/routes/application.js
+++ tests/dummy/app/routes/application.js
@@ -2,9 +2,4 @@ import { inject as service } from '@ember/service';
 import Route from '@ember/routing/route';
 
 export default class ApplicationRoute extends Route {
-  @service solidAuth;
-
-  async beforeModel() {
-    await this.solidAuth.restoreSession();
-  }
 }
